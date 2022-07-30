13698ab633fd026410b699e9c854424a61f6986e
Adrian Mrowka <mrowkaadrian@gmail.com>
2022-07-30 17:15:10 +0200

    You can now put products to the cart

--- my-app/src/App.svelte
+++ my-app/src/App.svelte
@@ -1,9 +1,22 @@
 <script>
+    let guid = 1;
+    let cart = [];
     let products = [
-        {name: "Apple", image: "assets/apple.jpg", price: 1},
-        {name: "Banana", image: "assets/banana.jpg", price: 2},
-        {name: "Orange", image: "assets/orange.jpg", price: 3}
+        {id: guid++, name: "Apple", image: "assets/apple.jpg", price: 1, quantity: 1},
+        {id: guid++, name: "Banana", image: "assets/banana.jpg", price: 2, quantity: 1},
+        {id: guid++, name: "Orange", image: "assets/orange.jpg", price: 3, quantity: 1}
     ];
+
+    const addToCart = (product) => {
+        for (let item of cart) {
+            if (item.id === product.id) {
+                item.quantity++;
+                cart = cart;
+                return
+            }
+        }
+        cart = [...cart, product]
+    }
 </script>
 
 <main>
@@ -13,10 +26,11 @@
                 <img src="{product.image}">
                 <h4>{product.name}</h4>
                 <p>{product.price}$</p>
-                <button>Add to cart</button>
+                <button on:click={() => addToCart(product)}>Add to cart</button>
             </div>
         {/each}
     </div>
+    Cart: {JSON.stringify(cart)}
 </main>
 
 <style>
